609e3fab5cfa0dec198bdc85cd0dfdec72191e37
JM <josegm1999@gmail.com>
2020-05-20 19:50:29 +0200

    first commit

---
+++ database/migrations/2020_05_20_173124_create_goals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('goalable');
+            $table->string('name');
+            $table->string('target')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('amount')->nullable()->default(1);
+            $table->integer('percent')->nullable()->default(5);
+            $table->dateTime('limit_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
---
+++ database/migrations/2020_05_20_173211_create_groups_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('target')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('amount')->nullable()->default(1);
+            $table->dateTime('limit_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
