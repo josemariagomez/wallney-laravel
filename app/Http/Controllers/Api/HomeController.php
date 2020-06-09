8b50d38a607f9e6a205190a937eb6e430de5c2ae
JM <josegm1999@gmail.com>
2020-06-09 21:01:33 +0200

    fix

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
         $user = auth()->user();
         $expenses = $user->monthExpenses();
         $incomes = $user->monthIncomes();
-        $saved = $incomes - $expenses;
+        $saved = intval($incomes) - intval($expenses);
         dd($saved);
         $main_card = [
             'ahorro' => number_format($saved, 2),
